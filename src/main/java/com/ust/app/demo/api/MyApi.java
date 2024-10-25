dcae25cdb783fb9f201c811166c559302f31d5bf
Amaan Ali Khan(UST <286176@ust.com>
2024-10-25 10:31:07 +0530

    initial-commit

---
+++ src/main/java/com/ust/app/demo/api/MyApi.java
@@ -0,0 +1,12 @@
+package com.ust.app.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyApi {
+    @GetMapping
+    public String sayHello(){
+        return "Hello world!";
+    }
+}
